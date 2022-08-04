7350cce105eae0b457288784ea0df4a6bab4206b
Alexander <alexandrkudryash@mail.ru>
2022-08-04 22:10:05 +0300

    создали код все четные числа от одного до N

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// вывести все четные числа от 1 до N
+
+Console.WriteLine("введите число");
+
+int N = int.Parse(Console.ReadLine());
+
+int a=0;
+while (a < N) 
+{
+if (a%2==0)
+    {
+    Console.WriteLine(a);
+        }
+    a=a+1;
+}
